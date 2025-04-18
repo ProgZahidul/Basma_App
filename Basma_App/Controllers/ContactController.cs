19919a7405a8cb62093afef4d226d676bc73dc0e
Zahidul Hoque <sayedzahidulhoquesaikat@gmail.com>
2025-04-18 11:43:48 +0600

    Add project files.

---
+++ Basma_App/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class ContactController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ContactController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(ContactViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Contacts.Add(model);
+            await _context.SaveChangesAsync();
+
+            ViewBag.SuccessMessage = "Your message has been submitted successfully.";
+            ModelState.Clear(); // Clear the form
+            return View(); // Return the same view
+        }
+
+        return View(model);
+    }
+
+}
---
+++ Basma_App/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿  // Models/ContactViewModel.cs
+    using System.ComponentModel.DataAnnotations;
+
+public class ContactViewModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Message { get; set; }
+}
